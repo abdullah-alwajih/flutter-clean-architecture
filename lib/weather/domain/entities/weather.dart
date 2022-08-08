8eab2a14293f1ca8474dcd41260a1d277d7d1413
abdullahAlwajih <pro.abdullah.alwajih@gmail.com>
2022-08-08 18:18:01 +0300

    Create Domain Layer (Entities)

---
+++ lib/weather/domain/entities/weather.dart
@@ -0,0 +1,15 @@
+class Weather {
+  final int id;
+  final String cityName;
+  final String main;
+  final String description;
+  final int pressure;
+
+  const Weather({
+    required this.id,
+    required this.cityName,
+    required this.main,
+    required this.description,
+    required this.pressure,
+  });
+}
