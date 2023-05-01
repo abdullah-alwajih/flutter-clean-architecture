e88830d17ec3eb9cfc0a28e3aa045dd250b48b24
Abdullah Al-Wajih <Pro.Abdullah.AlWajih@gmail.com>
2023-05-01 17:51:28 +0300

    build now playing item

--- lib/features/movies/presentation/views/components/now_playing.dart
+++ lib/features/movies/presentation/views/components/now_playing.dart
@@ -22,83 +22,9 @@ class NowPlayingComponent extends StatelessWidget {
                   height: 400.0,
                   viewportFraction: 1.0,
                 ),
-                items: state.nowPlayingMovies.map(
-                  (item) {
-                    return GestureDetector(
-                      onTap: () => Navigator.pushNamed(
-                          context, AppRoutes.movieDetails,
-                          arguments: item.id),
-                      child: Stack(
-                        children: [
-                          ShaderMask(
-                            shaderCallback: (rect) {
-                              return const LinearGradient(
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                                colors: [
-                                  // fromLTRB
-                                  Colors.transparent,
-                                  Colors.black,
-                                  Colors.black,
-                                  Colors.transparent,
-                                ],
-                                stops: [0, 0.3, 0.5, 1],
-                              ).createShader(
-                                Rect.fromLTRB(0, 0, rect.width, rect.height),
-                              );
-                            },
-                            blendMode: BlendMode.dstIn,
-                            child: cachedNetworkImage(
-                              ApiUrls.imageUrl(item.backdropPath),
-                              height: 560.0,
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(bottom: 16.0),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      const Icon(
-                                        Icons.circle,
-                                        color: Colors.redAccent,
-                                        size: 16.0,
-                                      ),
-                                      const SizedBox(width: 4.0),
-                                      Text(
-                                        AppLocalizations.of(context)!
-                                            .nowPlaying
-                                            .toUpperCase(),
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .titleMedium
-                                            ?.copyWith(letterSpacing: 0),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(bottom: 16.0),
-                                  child: Text(
-                                    item.title,
-                                    textAlign: TextAlign.center,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headlineSmall,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ).toList(),
+                items: state.nowPlayingMovies
+                    .map((item) => buildNowPlayingItem(context, item))
+                    .toList(),
               ),
             );
         }
