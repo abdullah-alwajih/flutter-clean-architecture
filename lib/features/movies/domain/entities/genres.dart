491ca4359504adb42b7682ecef9253fca9e31a14
Abdullah Al-Wajih <Pro.Abdullah.AlWajih@gmail.com>
2023-05-02 09:53:31 +0300

    generate movie, genres, recommendation and movie details adapters

--- lib/features/movies/domain/entities/genres.dart
+++ lib/features/movies/domain/entities/genres.dart
@@ -1,7 +1,13 @@
 import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
 
+part 'genres.g.dart';
+
+@HiveType(typeId: 2)
 class Genres extends Equatable {
+  @HiveField(0)
   final int id;
+  @HiveField(2)
   final String name;
 
   const Genres({
